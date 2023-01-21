2c6f64a3567a05e9f53e7503361dbc706aa95b07
Chanqun Sung <cksgns93@naver.com>
2023-01-21 14:20:34 +0900

    bootstrap, Nav 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import {Container, Nav, Navbar} from "react-bootstrap";
 
 function App() {
     return (
         <div className="App">
-
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="#home">Chanqun</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
         </div>
     );
 }
